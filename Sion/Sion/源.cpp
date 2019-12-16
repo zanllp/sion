59f848c16a439a2b6aa727bb1ce9294e5998cedb
zanllp <zanllp@163.com>
2019-12-16 15:10:01 +0800

    重写chunked文本的解析

--- Sion/Sion/源.cpp
+++ Sion/Sion/源.cpp
@@ -1,48 +1,73 @@
 #include <iostream>
-// #define SION_DISABLE_SSL
+#include <fstream>
+#include <functional>
+//#define SION_DISABLE_SSL
 #include "Sion.h"
 #include <ppltasks.h>
+#pragma warning(disable : 26444)
 using namespace Sion;
 using namespace std;
-int main()
+using namespace concurrency;
+
+
+void ErrorBoundaries(task<void> task)
+{
+	try
+	{
+		task.wait();
+	}
+	catch (const std::exception & e)
+	{
+		cout << "ERR!:  " << e.what() << endl;
+	}
+}
+
+task<void> RestTask()
 {
 	// 使用ppl实现异步操作，sion内为阻塞io
-	concurrency::create_task([]() {
-		try
-		{
-			auto rec = chrono::system_clock::now();
-			auto resp = Fetch("https://www.zanllp.cn/static/js/4.a50ac19e.chunk.js");
-			cout << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - rec).count() << "ms" << endl;
-			return resp;
-		}
-		catch (const std::exception & e)
-		{
-			cout << e.what();
-		}
-			return Response();
-	}).then([](Response resp) {
-			cout << resp.Source.length() << endl;
-	}).then([]{
-		try
-		{
-			auto rec = chrono::system_clock::now();
-			auto resp = Request()
-						.SetUrl("https://api.zanllp.cn/socket/push?descriptor=fHXMHCQfcgNHDq2P")
-						.SetHttpMethod(Method::Post)
-						.SetBody(R"({"data": 233333,"msg":"hello world!"})")
-						.SetHeader("Content-Type", "application/json; charset=utf-8")
-						.Send();
-			cout << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - rec).count() << "ms" << endl;
-			return resp;
-		}
-		catch (const std::exception & e)
-		{
-			cout << e.what();
-		}
-		return Response();
-	}).then([](Response resp) {
-		cout << resp.ResponseBody << endl;
-		exit(0);
-	});
-	this_thread::sleep_for(30s);
+	return create_task([] {cout << "-- REST任务开始" << endl; })
+		.then(([] { return Fetch("https://api.zanllp.cn/plugin"); }))
+		.then([](Response resp) {cout << "-- 响应头 Content-Type: " << resp.HeaderValue("content-type") << endl; })
+		.then(([]
+			{
+				return Request()
+					.SetUrl("https://api.zanllp.cn/socket/push?descriptor=fHXMHCQfcgNHDq2P")
+					.SetHttpMethod(Method::Post)
+					.SetBody(R"({"data": 233333,"msg":"hello world!"})")
+					.SetHeader("Content-Type", "application/json; charset=utf-8")
+					.Send();
+			}))
+		.then([](Response resp) { cout << "-- 响应体: " << resp.ResponseBody << endl; })
+		.then([] { return Fetch("https://www.themepark.com.cn/"); })
+		.then([](Response resp) { cout << "-- 分块text: " << resp.ContentLength << endl; })
+		.then(ErrorBoundaries); // 捕获运行时可能产生的异常
+}
+
+task<void> DownloadTask()
+{
+	auto DownloadChunkedFile = []
+	{
+		auto resp = Fetch("http://www.httpwatch.com/httpgallery/chunked/chunkedimage.aspx");
+		ofstream file(R"(分块.jpeg)", ios::binary);
+		file.write(resp.SourceChar.data(), resp.SourceChar.size() * sizeof(char));
+	};
+	auto DownloadHuaji = []
+	{
+		auto resp = Fetch("https://static.zanllp.cn/94da3a6b32e0ddcad844aca6a8876da2ecba8cb3c7094c3ad10996b28311e4b50ab455ee3d6d55fb50dc4e3c62224f4a20a4ddb1.gif");
+		ofstream file(R"(滑稽.gif)", ios::binary);
+		file.write(resp.SourceChar.data(), resp.SourceChar.size() * sizeof(char));
+	};
+	return create_task([] { cout << "下载任务开始" << endl; })
+		.then(DownloadChunkedFile)
+		.then([] { cout << "分块文件下载完成" << endl; })
+		.then(DownloadHuaji)
+		.then([] { cout << "滑稽.gif下载完成" << endl; })
+		;// .then(ErrorBoundaries);
+}
+
+
+int main()
+{
+	vector<task<void>> tasks{ DownloadTask(),RestTask() };
+	when_all(tasks.begin(), tasks.end()).wait();
 }
